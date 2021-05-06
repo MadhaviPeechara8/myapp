8677720076fe83069364f43a92a101d20e707658
--replace-all <--replace-all>
2021-05-06 20:10:39 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyPracticeApp1';
+  /*arr:string[]=[];
+  sendData(d){
+    this.arr.push(d);
+  }
+  status:boolean=true;
+  changeStatus(){
+    this.status=!this.status;
+  }*/
+  //Assignment-2
+  users=[];
+  addUserData(ref:NgForm){
+    let userObject=ref.value;
+    //push into user array
+    this.users.push(userObject);
+   //console.log(userObject);
+    ref.reset();
+  }
+  deleteUser(ind){
+    this.users.splice(ind,1);
+  }
 }
